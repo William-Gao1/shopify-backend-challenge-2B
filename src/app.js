686e7c5eb66f45fcd63606ffdb7210cd0a6594df
WilliamGao123 <48528037+William-Gao1@users.noreply.github.com>
2022-05-11 22:21:11 +0000

    Added open api docs

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const inventoryRouter = require('./router/v1/inventoryRouter');
 const shipmentRouter = require('./router/v1/shipmentRouter');
 
 app.use(express.json())
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openAPIDocs));
+app.use('/', swaggerUi.serve, swaggerUi.setup(openAPIDocs));
 app.use('/v1/inventory', inventoryRouter);
 app.use('/v1/shipment', shipmentRouter);
 
